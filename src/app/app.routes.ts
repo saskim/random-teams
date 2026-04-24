6ad24c2941f37cf02735f51a77368904967c1d0c
Sonja Askim <sonja.askim@gmail.com>
2026-04-24 10:59:13 +0200

    chore: migrate to route lazy loading
    `ng generate @angular/core:route-lazy-loading`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,27 @@
 import { type Routes } from '@angular/router';
 
-import { PlayersComponent } from './players/players.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
-import { TeamsComponent } from './teams/teams.component';
-import { TournamentsComponent } from './tournaments/tournaments.component';
-
 export const routes: Routes = [
   {
     path: 'players',
-    component: PlayersComponent,
-    data: { title: 'Players' }
+    loadComponent: () => import('./players/players.component').then((m) => m.PlayersComponent),
+    data: { title: 'Players' },
   },
   {
     path: 'teams',
-    component: TeamsComponent,
-    data: { title: 'Teams' }
+    loadComponent: () => import('./teams/teams.component').then((m) => m.TeamsComponent),
+    data: { title: 'Teams' },
   },
   {
     path: 'matches',
-    component: TournamentsComponent,
-    data: { title: 'Matches' }
+    loadComponent: () =>
+      import('./tournaments/tournaments.component').then((m) => m.TournamentsComponent),
+    data: { title: 'Matches' },
   },
   {
     path: 'scoreboard',
-    component: ScoreboardComponent,
-    data: { title: 'Scoreboard' }
+    loadComponent: () =>
+      import('./scoreboard/scoreboard.component').then((m) => m.ScoreboardComponent),
+    data: { title: 'Scoreboard' },
   },
   { path: '', redirectTo: 'players', pathMatch: 'full' },
 ];
